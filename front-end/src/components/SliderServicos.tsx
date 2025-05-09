856274ac6a28b9537f2e6a938ef21ea58b4b276f
Weslei Casali <wesleicasali18@gmail.com>
2025-05-09 11:51:40 -0300

    att

--- front-end/src/components/SliderServicos.tsx
+++ front-end/src/components/SliderServicos.tsx
@@ -91,7 +91,7 @@ const SliderServicos = () => {
                 {servicos[servicoAtivo].icone}
               </div>
               
-              <h3 className="text-2xl md:text-3xl font-bold mb-4">
+              <h3 className="text-2xl md:text-3xl text-center md:text-start font-bold mb-4">
                 {servicos[servicoAtivo].titulo}
               </h3>
               
